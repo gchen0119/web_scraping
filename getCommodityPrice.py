059ff48b6445da4330e35137bd0d7e830fc4e09c
gchen <gchen@rsmas.miami.edu>
2018-12-09 09:21:54 -0800

    update step 2

--- getCommodityPrice.py
+++ getCommodityPrice.py
@@ -1,5 +1,18 @@
-def getCommodityPrice(start_date,end_date,commodity):
-    df = pd.DataFrame.from_csv('./data/data.csv')
-    mask = (df['date']>=start_date) & (df['date']<=end_date) # alternatively, df[df.some_date.between(start_date, end_date)]
+# run this program in command line as
+# Example:
+# python getCommodityPrice 2018-11-07 2018-12-01 gold 
+
+
+
+import sys
+import pandas as pd
+def getCommodityPrice():
+    df = pd.read_csv('./data/data.csv')
+    mask = (df['date']>=sys.argv[1]) & (df['date']<=sys.argv[2]) # alternatively, df[df.some_date.between(start_date, end_date)]
+    if not any(mask):
+        raise ValueError("Date out of range %s and %s" % (df['date'].iloc[-1],df['date'].iloc[0]))
     df = df.loc[mask]
-    return commodity, df[commodity].mean(), df[commodity].std()
+    print(sys.argv[3], df[sys.argv[3]].mean(), df[sys.argv[3]].std())
+
+if __name__ == '__main__': # if module executed directly, then run main()
+    getCommodityPrice()
